0cf2deb3dd3282db5c9fcbea7f6e6ed5be3c2be4
Muruganandhan <muruganandhan_p@yahoo.com>
2020-04-21 06:03:40 +0530

    Added healthchk endpoint

--- cidemo/apps/views.py
+++ cidemo/apps/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+
+def healthchk(request, *args, **kwargs):
+    return HttpResponse(status=200)
